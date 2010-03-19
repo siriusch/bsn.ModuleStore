5e0cb2e05d10ab9f6af1b9246885f505bbd1f7e4
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-03-19 17:39:57 +0000

    Added unit test projects and some simple tests

--- bsn.ModuleStore.Test/Properties/AssemblyInfo.cs
+++ bsn.ModuleStore.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("bsn.ModuleStore")]
+[assembly: AssemblyTitle("bsn.ModuleStore.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("bsn.ModuleStore")]
+[assembly: AssemblyProduct("bsn.ModuleStore.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,11 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9fbb450c-47b2-4ad4-ae69-a5476367c2c5")]
-[assembly:
-		InternalsVisibleTo(
-				"bsn.ModuleStore.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100230eb4221fcce3d50a5806f69855416c597d5ddf8284ef7f0d29a72d8f8ca0179da1b61694cdfc22bbdd8af8f505ca3bbf1f9ab92cacf7fc6c45111dcffeef998dfbfb47f8064cd5fe3caf14114a13c5c8acb8a0ae6ea2f79ad673ddd33ec594dec2bdeeb7f562b1576949a41b44cdd4791f06a1fa44d3d2148df163e89498af"
-				)]
+[assembly: Guid("2c26b3e5-2105-4e4c-8765-a6d4dfa9a02c")]
 
 // Version information for an assembly consists of the following four values:
 //
