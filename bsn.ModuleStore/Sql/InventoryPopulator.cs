2bd6c39dc8b5099da92833701df8af149e56fdae
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-03-25 03:14:04 +0100

    More fixes to grammar for table and column constraints

--- bsn.ModuleStore/Sql/InventoryPopulator.cs
+++ bsn.ModuleStore/Sql/InventoryPopulator.cs
@@ -1,25 +1,34 @@
 ﻿// (C) 2010 Arsène von Wyss / bsn
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Xml.Linq;
 
 using bsn.GoldParser.Xml;
+using bsn.ModuleStore.Sql.Definitions;
 
 namespace bsn.ModuleStore.Sql {
 	public class InventoryPopulator {
-		public void ProcessScript(XElement doc) {
+		public void IEnumerable<SqlObject> ProcessScript(XElement doc) {
 			foreach (XElement element in doc.Elements("Statement")) {
 				XAttribute statementKind = element.Attribute("Kind");
 				Debug.Assert(statementKind != null);
 				switch (statementKind.Value) {
 				case "CreateTable":
-				case "AlterTable":
+					yield return CreateTable(element);
 				case "CreateIndex":
+					yield return CreateIndex(element);
 				case "CreateFulltext":
+					yield return CreateFulltextIndex(element);
 				case "CreateFunction":
+					yield return CreateFunction(element);
 				case "CreateProcedure":
+					yield return CreateProcedure(element);
 				case "CreateView":
+					yield return CreateView(element);
+				default:
+					throw new NotSupportedException("SQL create scripts expected");
 				}
 			}
 		}
