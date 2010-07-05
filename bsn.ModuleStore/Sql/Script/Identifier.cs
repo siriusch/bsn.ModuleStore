b15d4c9579040fe80bf221d5cccf7c17d47f8484
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-07-05 15:21:44 +0200

    Introduced more classes to reflect SQL semantics

---
+++ bsn.ModuleStore/Sql/Script/Identifier.cs
@@ -0,0 +1,10 @@
+using System;
+
+using bsn.GoldParser.Semantic;
+
+namespace bsn.ModuleStore.Sql.Script {
+	[Terminal("Id")]
+	public class Identifier: SqlIdentifier {
+		public Identifier(string id): base(id) {}
+	}
+}
