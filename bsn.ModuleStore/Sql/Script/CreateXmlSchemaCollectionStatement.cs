3213413de47b912cce5f125de867fa467e931c97
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-08-05 11:44:39 +0200

    More work on SQL grammar AST (TSQL XML schema collections)

---
+++ bsn.ModuleStore/Sql/Script/CreateXmlSchemaCollectionStatement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using bsn.GoldParser.Semantic;
+
+namespace bsn.ModuleStore.Sql.Script {
+	public class CreateXmlSchemaCollectionStatement: SqlCreateStatement {
+		private readonly XmlSchemaCollectionName xmlSchemaCollectionName;
+		private readonly Expression expression;
+
+		[Rule("<CreateXmlSchemaCollectionStatement> ::= CREATE XML_SCHEMA_COLLECTION <XmlSchemaCollectionName> AS <Expression>", ConstructorParameterMapping = new[] {2, 4})]
+		public CreateXmlSchemaCollectionStatement(XmlSchemaCollectionName xmlSchemaCollectionName, Expression expression) {
+			if (xmlSchemaCollectionName == null) {
+				throw new ArgumentNullException("xmlSchemaCollectionName");
+			}
+			if (expression == null) {
+				throw new ArgumentNullException("expression");
+			}
+			this.xmlSchemaCollectionName = xmlSchemaCollectionName;
+			this.expression = expression;
+		}
+	}
+}
---
+++ bsn.ModuleStore/Sql/Script/DropXmlSchemaCollectionStatement.cs
@@ -0,0 +1,8 @@
+﻿using bsn.GoldParser.Semantic;
+
+namespace bsn.ModuleStore.Sql.Script {
+	public class DropXmlSchemaCollectionStatement: SqlDropStatement {
+		[Rule("<DropXmlSchemaCollectionStatement> ::= DROP XML_SCHEMA_COLLECTION <XmlSchemaCollectionName>", ConstructorParameterMapping = new[] {2})]
+		public DropXmlSchemaCollectionStatement(XmlSchemaCollectionName xmlSchemaCollectionName) {}
+	}
+}
