b4ba9aa054a4f8c096277a4fdff340120d62e2ac
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-08-06 13:48:07 +0200

    Fixed syntax for column default values

--- bsn.ModuleStore/Sql/Script/ColumnDefaultConstraint.cs
+++ bsn.ModuleStore/Sql/Script/ColumnDefaultConstraint.cs
@@ -4,7 +4,10 @@ using bsn.GoldParser.Semantic;
 
 namespace bsn.ModuleStore.Sql.Script {
 	public class ColumnDefaultConstraint: ColumnConstraint {
-		[Rule("<ColumnConstraint> ::= DEFAULT '(' <Expression> ')'", ConstructorParameterMapping = new[] {2})]
+		[Rule("<ColumnConstraint> ::= DEFAULT '(' <FunctionCall> ')'", ConstructorParameterMapping = new[] {2})]
+		[Rule("<ColumnConstraint> ::= DEFAULT <NumberLiteral>", ConstructorParameterMapping = new[] {1})]
+		[Rule("<ColumnConstraint> ::= DEFAULT <StringLiteral>", ConstructorParameterMapping = new[] {1})]
+		[Rule("<ColumnConstraint> ::= DEFAULT <NullLiteral>", ConstructorParameterMapping = new[] {1})]
 		public ColumnDefaultConstraint(Expression defaultValue) {}
 	}
 }
---
+++ bsn.ModuleStore/Sql/Script/NullLiteral.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using bsn.GoldParser.Semantic;
+
+namespace bsn.ModuleStore.Sql.Script {
+	public class NullLiteral: Literal {
+		[Rule("<NullLiteral> ::= NULL", AllowTruncationForConstructor = true)]
+		public NullLiteral() {}
+	}
+}
