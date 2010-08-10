7f3432c5f0b8ccdc1f0ef39d37f3da459c0095f7
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-08-11 00:48:24 +0200

    Red: Grammar enhanced to allow MAXRECURSION option, fixed regex in quoted name

--- bsn.ModuleStore/Sql/Script/SqlQuotedName.cs
+++ bsn.ModuleStore/Sql/Script/SqlQuotedName.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 namespace bsn.ModuleStore.Sql.Script {
 	public abstract class SqlQuotedName: SqlName {
 		private static readonly char[] brackets = new[] {']', '['};
-		private static readonly Regex rxUnquote = new Regex(@"^(\[(?<name>[^\[\]]+)\]|""(?<name>[^""]+""|(?<name>\S+)$", RegexOptions.CultureInvariant|RegexOptions.ExplicitCapture);
+		private static readonly Regex rxUnquote = new Regex(@"^(\[(?<name>[^\[\]]+)\]|""(?<name>[^""]+)""|(?<name>\S+))$", RegexOptions.CultureInvariant|RegexOptions.ExplicitCapture);
 
 		protected SqlQuotedName(string name): base(rxUnquote.Match(name).Groups["name"].Value) {}
 
