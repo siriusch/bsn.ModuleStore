aa4e8124a52c0b300b7a508ad8985912e88d8cba
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-09-08 12:27:59 +0200

    Splitted the bsn.ModuleStore assembly into a parser and the rest in order to boost the performance when reflecting over all types of the assembly

---
+++ bsn.ModuleStore.Parser/Properties/AssemblyInfo.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("bsn.ModuleStore.Parser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("bsn.ModuleStore.Parser")]
+[assembly: AssemblyCopyright("Copyright © Arsène von Wyss 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d0cab09-7583-4114-b047-f31eb921930e")]
+
+[assembly:
+		InternalsVisibleTo(
+				"bsn.ModuleStore.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100230eb4221fcce3d50a5806f69855416c597d5ddf8284ef7f0d29a72d8f8ca0179da1b61694cdfc22bbdd8af8f505ca3bbf1f9ab92cacf7fc6c45111dcffeef998dfbfb47f8064cd5fe3caf14114a13c5c8acb8a0ae6ea2f79ad673ddd33ec594dec2bdeeb7f562b1576949a41b44cdd4791f06a1fa44d3d2148df163e89498af"
+				)]
+[assembly:
+		InternalsVisibleTo(
+				"bsn.ModuleStore, PublicKey=0024000004800000940000000602000000240000525341310004000001000100230eb4221fcce3d50a5806f69855416c597d5ddf8284ef7f0d29a72d8f8ca0179da1b61694cdfc22bbdd8af8f505ca3bbf1f9ab92cacf7fc6c45111dcffeef998dfbfb47f8064cd5fe3caf14114a13c5c8acb8a0ae6ea2f79ad673ddd33ec594dec2bdeeb7f562b1576949a41b44cdd4791f06a1fa44d3d2148df163e89498af"
+				)]
+[assembly:
+		InternalsVisibleTo(
+				"ModuleStore, PublicKey=0024000004800000940000000602000000240000525341310004000001000100230eb4221fcce3d50a5806f69855416c597d5ddf8284ef7f0d29a72d8f8ca0179da1b61694cdfc22bbdd8af8f505ca3bbf1f9ab92cacf7fc6c45111dcffeef998dfbfb47f8064cd5fe3caf14114a13c5c8acb8a0ae6ea2f79ad673ddd33ec594dec2bdeeb7f562b1576949a41b44cdd4791f06a1fa44d3d2148df163e89498af"
+				)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
