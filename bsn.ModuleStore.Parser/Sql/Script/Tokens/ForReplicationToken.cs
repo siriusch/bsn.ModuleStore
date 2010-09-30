886a162dd1242454c5335aa0c202e6b63410660a
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-09-30 16:44:17 +0200

    Changed the "special keywords" into normal keywords which can be converted to Identiy tokens if syntax errors occur, changed source accordingly, removed some enumerations

---
+++ bsn.ModuleStore.Parser/Sql/Script/Tokens/ForReplicationToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using bsn.GoldParser.Semantic;
+
+namespace bsn.ModuleStore.Sql.Script.Tokens {
+	public sealed class ForReplicationToken: ReplicationToken {
+		[Rule("<ProcedureFor> ::= ~FOR ~REPLICATION")]
+		public ForReplicationToken() {}
+
+		public override bool? ForReplication {
+			get {
+				return true;
+			}
+		}
+
+		public override void WriteTo(SqlWriter writer) {
+			writer.Write("FOR REPLICATION");
+		}
+	}
+}
