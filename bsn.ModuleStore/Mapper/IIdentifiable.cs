bfc8725c24712238707e927303362c6f4ca5fdcb
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-10-11 11:58:08 +0200

    Added IIdentifiabe interface

---
+++ bsn.ModuleStore/Mapper/IIdentifiable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace bsn.ModuleStore.Mapper {
+	public interface IIdentifiable<T> where T: struct, IEquatable<T>, IConvertible {
+		T Id {
+			get;
+		}
+	}
+}
